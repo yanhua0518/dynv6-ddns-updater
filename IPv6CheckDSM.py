9146190f39f11fcad2b100496ccb9b8a219c006d
yanhua0518 <42855794+yanhua0518@users.noreply.github.com>
2023-03-11 13:42:52 +0800

    Add files via upload

---
+++ IPv6CheckDSM.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+from datetime import datetime
+from urllib.parse import urlencode
+from urllib.request import urlopen
+
+def updateIPv6(hostname,token,ip):
+    #addr=ip[:ip.rfind('/')]
+    #prefix=ip[ip.rfind('/')+1:]
+    parameters={'hostname':hostname,'token':token,'ipv6':ip}
+    url='https://dynv6.com/api/update?'
+    url+=urlencode(parameters)
+    try:
+        res=urlopen(url)
+        data=res.read().decode()
+    except Exception as e:
+    	return str(e)
+    else:
+    	return data
+
+
+def oldIPv6(fileName):
+    try:
+        f=open(fileName,'r')
+        ip=f.readline()
+        f.close()
+    except FileNotFoundError:
+        return ""
+    except Exception as e:
+        return e
+    else:
+        return ip
+
+def saveIPv6(fileName,ip):
+    try:
+        f=open(fileName,'w')
+        f.write(ip)
+        f.close()
+    except Exception as e:
+        return e
+    else:
+        return True
+
+def findIPv6():
+    output=os.popen('ip -f inet6 addr show eth0').readlines()
+    ip=''
+    for line in output:
+        if line.find('global')>-1:
+            ip=line[line.find('inet6')+6:line.find('/')]
+            #ip=line[line.find('inet6')+6:line.find('scope')-1]
+            break
+    if ip:
+        return ip
+    else:
+        return False
+
+def saveLog(fileName,log):
+    try:
+        f=open(fileName,'a+')
+        f.write(log)
+        f.close()
+    except Exception as e:
+        return e
+    else:
+        return True
+
+SAVED_IP="IPv6.txt" #本地保存IP地址的文件
+IP_LOG="IPlog.txt" #本地记录文件
+HOSTNAME="sample.dynv6.net" #hostname 
+TOKEN="" #token
+
+tempIP=findIPv6()
+if tempIP:
+    oldIP=oldIPv6(SAVED_IP)
+    if tempIP!=oldIP:
+        newIP=tempIP
+        sentlog=datetime.now().strftime("%Y-%m-%d %H:%M:%S")+"\tNotice: IPv6 changed\n"
+        import smtplib
+        from email.mime.text import MIMEText
+        msg=MIMEText(newIP,'plain','utf-8')
+        msg['From']='a@qq.com' #发件人
+        msg['To']='b@qq.com' #收件人
+        msg['Subject']='IP changed' #邮件主题
+        text=msg.as_string()
+        try:
+            smtp=smtplib.SMTP('smtp.qq.com',25) #SMTP服务器地址、端口
+            smtp.login('username','password') #用户名或邮箱地址、密码或授权码
+            smtp.sendmail("a@qq.com","b@qq.com",text) #发件地址、收件地址
+        except Exception as e:
+            sentlog+=datetime.now().strftime("%Y-%m-%d %H:%M:%S")+'\tE-mail: '+str(e)+'\n'
+        #else:
+            #sentlog+=datetime.now().strftime("%Y-%m-%d %H:%M:%S")+"\tE-mail: sent\n"
+        check=saveIPv6(SAVED_IP,newIP)
+        if check!=True:
+            sentlog+=datetime.now().strftime("%Y-%m-%d %H:%M:%S")+'\tfile: '+str(check)+'\n'
+        #else:
+            #sentlog+=datetime.now().strftime("%Y-%m-%d %H:%M:%S")+"\tfile: new IPv6 saved\n"
+        res=updateIPv6(HOSTNAME,TOKEN,newIP)
+        sentlog+=datetime.now().strftime("%Y-%m-%d %H:%M:%S")+'\tDDNS: '+res+'\n'
+        saveLog(IP_LOG,sentlog)
+    else:
+        print("IPv6 no change")
+else:
+    sentlog=datetime.now().strftime("%Y-%m-%d %H:%M:%S")+"\tglobal IPv6 not found!\n"
+    saveLog(IP_LOG,sentlog)
---
+++ IPv6CheckWin.py
@@ -0,0 +1,134 @@
+# -*- coding: utf-8 -*-
+
+import netifaces
+import time
+from datetime import datetime
+from urllib.parse import urlencode
+from urllib.request import urlopen
+import win32serviceutil
+import win32service
+import win32event
+
+
+def getInterface(): #确认网卡连接公网的网卡
+    interfaces=netifaces.interfaces()
+    for interface in interfaces:
+        addr=netifaces.ifaddresses(interface)
+        if netifaces.AF_INET6 in addr.keys():
+            tempIP=addr[netifaces.AF_INET6][0]['addr']
+            if tempIP[:4]=='2409':
+                return interface
+    return ''
+
+def findIPv6(interface):
+    addrs=netifaces.ifaddresses(interface)
+    #ip=addrs[netifaces.AF_INET6][0]['addr'] #获取最靠上的地址
+    ip=''
+    for addr in addrs[netifaces.AF_INET6]: #获取最新的临时地址
+        if addr['addr'][:4]=='2409':
+            ip=addr['addr']
+    return ip
+
+def updateIPv6(hostname,token,ip):
+    #addr=ip[:ip.rfind('/')]
+    #prefix=ip[ip.rfind('/')+1:]
+    parameters={'hostname':hostname,'token':token,'ipv6':ip}
+    url='https://dynv6.com/api/update?'
+    url+=urlencode(parameters)
+    try:
+        res=urlopen(url)
+        data=res.read().decode()
+    except Exception as e:
+    	return str(e)
+    else:
+    	return data
+
+def oldIPv6(fileName):
+    try:
+        f=open(fileName,'r')
+        ip=f.readline()
+        f.close()
+    except FileNotFoundError:
+        return ""
+    except Exception as e:
+        return e
+    else:
+        return ip
+
+def saveIPv6(fileName,ip):
+    try:
+        f=open(fileName,'w')
+        f.write(ip)
+        f.close()
+    except Exception as e:
+        return e
+    else:
+        return True
+
+def saveLog(fileName,log):
+    try:
+        f=open(fileName,'a+')
+        f.write(log)
+        f.close()
+    except Exception as e:
+        return e
+    else:
+        return True
+
+
+
+
+class AppServerSvc(win32serviceutil.ServiceFramework):
+    _svc_name_ = "IPv6Check"
+    _svc_display_name_ = "IPv6 DDNS updater"
+    _svc_description_ = "Update IPv6 to DDNS service"
+
+    def __init__(self,args):
+        win32serviceutil.ServiceFramework.__init__(self,args)
+        self.hWaitStop = win32event.CreateEvent(None,0,0,None)
+
+    def SvcStop(self):
+        self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
+        win32event.SetEvent(self.hWaitStop)
+
+    def SvcDoRun(self):
+        SAVED_IP="C:/Users/sample/OneDrive/IPv6.txt" #本地保存IP地址的文件
+        SLEEP_TIME=10 #运行间隔，单位分钟
+        INTERFACE=getInterface()
+        oldIP=oldIPv6(SAVED_IP)
+        loop=True
+        while loop:
+            oldIP=self.oneLoop(oldIP,INTERFACE)
+            #time.sleep(SLEEP_TIME*60) 
+            if win32event.WaitForSingleObject(self.hWaitStop,SLEEP_TIME*60*1000)==win32event.WAIT_OBJECT_0:
+                break
+    
+    def oneLoop(self,oldIP,interface):
+        SAVED_IP="C:/Users/sample/OneDrive/IPv6.txt" #本地保存IP地址的文件
+        IP_LOG="C:/Users/sample/OneDrive/IPlog.txt" #本地记录文件
+        HOSTNAME="sample.dynv6.net" #hostname 
+        TOKEN="" #token
+        tempIP=findIPv6(interface)
+        if tempIP!=oldIP:
+            newIP=tempIP
+            sentlog=datetime.now().strftime("%Y-%m-%d %H:%M:%S")+" Notice:\tIPv6 changed\n"
+            sentlog+=datetime.now().strftime("%Y-%m-%d %H:%M:%S")+" IPv6:\t"+newIP+"\n"
+            saveLog(IP_LOG,sentlog)
+            check=saveIPv6(SAVED_IP,newIP)
+            if check!=True:
+                sentlog=datetime.now().strftime("%Y-%m-%d %H:%M:%S")+' File:\t'+str(check)+'\n'
+            else:
+                sentlog=datetime.now().strftime("%Y-%m-%d %H:%M:%S")+" File:\tnew IPv6 saved\n"
+            saveLog(IP_LOG,sentlog)
+            res=updateIPv6(HOSTNAME,TOKEN,newIP)
+            sentlog=datetime.now().strftime("%Y-%m-%d %H:%M:%S")+' DDNS:\t'+res+'\n'
+            saveLog(IP_LOG,sentlog)
+            return newIP
+        else:
+            #sentlog=datetime.now().strftime("%Y-%m-%d %H:%M:%S")+' Info:\tIPv6 no change\n'
+            #saveLog(IP_LOG,sentlog)
+            return oldIP
+
+if __name__ == '__main__':
+    win32serviceutil.HandleCommandLine(AppServerSvc)
+
